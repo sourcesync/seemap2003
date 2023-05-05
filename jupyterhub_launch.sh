ebefe80dc7fecf15c90a9d383010d1566e748bcd
George Williams <george.williams@gmail.com>
2023-05-04 19:06:17 -0700

    cleaning up script

--- jupyterhub_launch.sh
+++ jupyterhub_launch.sh
@@ -1,15 +1,7 @@
 #!/bin/zsh
 
-
-# Check if it's already running
-JUPYTER_RUNNING=$(pgrep jupyterhub)
-if [ ! -z "$JUPYTER_RUNNING" ]
-then
-    echo "Jupyterhub is already running..."
-else
-    echo "Launch Jupyterhub..."
-    source ~/.zshrc
-    conda activate seemap2023
-    sudo rm jupyterhub.sqlite jupyterhub_cookie_secret
-    sudo jupyterhub --config=jupyterhub_config.py
-fi
+echo "Launch Jupyterhub..."
+source ~/.zshrc
+conda activate seemap2023
+sudo rm jupyterhub.sqlite jupyterhub_cookie_secret
+sudo jupyterhub --config=jupyterhub_config.py
