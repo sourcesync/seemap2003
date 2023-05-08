a2b3d9d46c2410c3d83563d1e7f2acca4209b665
George Williams <george.williams@gmail.com>
2023-05-08 13:04:50 -0700

    now producing versioned tarballs

--- package_data.sh
+++ package_data.sh
@@ -7,10 +7,20 @@ set -x
 
 # script starts here
 
+# Cleanup previous data copy
+rm -fr ../data_transfer ../data_transfer.tar ../data_transfer.tar.gz
 mkdir -p ../data_transfer
+
+# Copy data files
 cp -fr ./data ../data_transfer/
 mkdir -p ../data_transfer/.cache
-cp -fr ~/.cache/huggingface ../data_transfer/.cache/
+#cp -fr ~/.cache/huggingface ../data_transfer/.cache/
 cp -fr ~/.cache/torch ../data_transfer/.cache/
 
-cd .. && tar cvf data_transfer.tar data_transfer/ && gzip data_transfer.tar
+# Get timestamp for now
+DT=$(date +%s)
+
+# Create tarball
+cd .. && tar cvf "data_transfer_$DT.tar" data_transfer/ && gzip "data_transfer_$DT.tar"
+
+echo "Created tarball data_transfer_$DT.tar."
