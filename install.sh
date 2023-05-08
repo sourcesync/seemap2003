b76b21237cb875306a7d58225ccab2e9bd127dcd
George Williams <george.williams@gmail.com>
2023-05-07 18:01:25 -0700

    removing various dot dirs from workshop users

--- install.sh
+++ install.sh
@@ -121,17 +121,20 @@ do
             set +e
             sudo rm -f $USER_DIR/*.ipynb
             sudo rm -f $USER_DIR/*.py 
-            sudo rm -r $USER_DIR/__pycache__
+            sudo rm -fr $USER_DIR/__pycache__
+            sudo rm -fr $USER_DIR/.ipython
+            sudo rm -fr $USER_DIR/.jupyter
+            sudo rm -fr $USER_DIR/.ipynb_checkpoints
             set -e
             # copy workshop files and change permissions as needed
             sudo cp *.ipynb survival_analysis.py $USER_DIR/
             sudo chmod ugo+r $USER_DIR/*.ipynb $USER_DIR/*.py
             sudo chmod ugo-w $USER_DIR/*.ipynb $USER_DIR/*.py
             sudo cp -fr images $USER_DIR/
-            sudo cp -fr data $USER_DIR/
+            sudo cp -fr ../data_transfer/* $USER_DIR/data/
             sudo chmod -R ugo+rw $USER_DIR/data
-            sudo cp -fr ~/.cache/huggingface $USER_DIR/.cache/
-            sudo cp -fr ~/.cache/torch $USER_DIR/.cache/
+            sudo cp -fr ../data_transfer/.cache/huggingface $USER_DIR/.cache/
+            sudo cp -fr ../data_transfer/.cache/torch $USER_DIR/.cache/
             sudo chmod -R ugo+rw $USER_DIR/.cache
         fi    
         echo $USER
