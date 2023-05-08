b646a6fd9f17a2960d02034b363d25bb817ccb25
George Williams <george.williams@gmail.com>
2023-05-07 17:51:03 -0700

    script to package up bin data

---
+++ package_data.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# config
+set -e
+
+set -x
+
+# script starts here
+
+mkdir -p ../data_transfer
+cp -fr ./data ../data_transfer/
+mkdir -p ../data_transfer/.cache
+cp -fr ~/.cache/huggingface ../data_transfer/.cache/
+cp -fr ~/.cache/torch ../data_transfer/.cache/
+
+tar cvf ../data_transfer.tar ../data_transfer
+gzip ../data_transfer.tar
