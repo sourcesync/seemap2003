8240a9c9a46e3514b1b97197ab1f4f552a0b74d9
George Williams <george.williams@gmail.com>
2023-05-11 13:38:43 -0700

    leverage nbconvert to clear output cells

--- package_data.sh
+++ package_data.sh
@@ -8,30 +8,36 @@ set -x
 # script starts here
 
 #
-# mac-mini data files
+# package mac-mini data files
 #
 
-# Cleanup previous data copy
+# Cleanup previous state
 rm -fr ../data_transfer ../data_transfer.tar ../data_transfer.tar.gz
 mkdir -p ../data_transfer
 
-# Copy notebooks
-cp 02_seemapld2023_survival.ipynb ../data_transfer/
-cp 04_seemapld2023_fastai_mnist.ipynb ../data_transfer/
+# Copy notebook first clearing output cells
+jupyter nbconvert --clear-output --to notebook --output=../data_transfer/02_seemapld2023_survival.ipynb 02_seemapld2023_survival.ipynb
+jupyter nbconvert --clear-output --to notebook --output=../data_transfer/04_seemapld2023_fastai_mnist.ipynb 04_seemapld2023_fastai_mnist.ipynb
+#cp 02_seemapld2023_survival.ipynb ../data_transfer/
+#cp 04_seemapld2023_fastai_mnist.ipynb ../data_transfer/
+
 # Copy workspace data files
 cp -fr ./data ../data_transfer/
+
 # Copy torch cache (models)
 mkdir -p ../data_transfer/.cache/torch/hub/checkpoints
 cp -fr ~/.cache/torch/hub/checkpoints/mobilenetv3_small* ../data_transfer/.cache/torch/hub/checkpoints/
 cp -fr ~/.cache/torch/hub/checkpoints/squeeze* ../data_transfer/.cache/torch/hub/checkpoints/
+
 # Copy fastai images (tgz only and config.ini)
 cp -fr ~/.fastai/archive/mnist_png.tgz ../data_transfer/
 cp -fr ~/.fastai/config.ini ../data_transfer
+
 # Copy jupyter configs
 cp ~/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/themes.jupyterlab-settings ../data_transfer/
 
 #
-# nano data filee
+# package nano data filee
 #
 
 # Cleanup previous data copy
@@ -39,7 +45,8 @@ rm -fr ../data_nano ../data_nano.tar ../data_nano.tar.gz
 mkdir -p ../data_nano
 
 # Copy data files
-cp -fr ./data/hymenoptera_data_val_small ../data_nano
+#cp -fr ./data/hymenoptera_data_val_small ../data_nano/
+cp -fr ~/.fastai/archive/mnist_png.tgz ../data_nano/
 
 #
 # make the tarballs
