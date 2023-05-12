9ff5a747aadcba9b923700cf2c36a939ed41ddea
George Williams <george.williams@gmail.com>
2023-05-12 14:33:25 -0700

    new speed test with nano address support

--- test_model_speed_on_nano.sh
+++ test_model_speed_on_nano.sh
@@ -1,9 +1,31 @@
-#!/bin/bash
+#!/bin/zsh
 
 MODEL=$1
 
-echo "Copying your model to nano..."
-scp "${MODEL}" cuongwilliams@192.168.2.13:/home/cuongwilliams/
+NANO=$2
 
-echo "Running model speed test on nano..."
-ssh cuongwilliams@192.168.2.13 -t /home/cuongwilliams/nano_model_test.sh "${MODEL}"
+if [ -f "${MODEL}" ]
+then
+    echo "Found your model "${MODEL}"
+    echo
+else
+    echo "ERROR: Could not find your model ${MODEL}"
+    exit 1
+fi
+
+if [ -z "${NANO}" ]
+then
+    NUM=$(awk -v min=5 -v max=10 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+    NANO="nano${NUM}"
+    echo "Randomly choosing the nano called ${NANO}"
+else
+    echo "Using the nano called ${NANO}"
+fi
+echo
+
+echo "Copying your model to the nano..."
+scp "${MODEL}" "cuongwilliams@${NANO}:/home/cuongwilliams/"
+echo
+
+echo "Running model speed test on the nano..."
+ssh "cuongwilliams@${NANO} -t /home/cuongwilliams/nano_model_test.sh "${MODEL}"
