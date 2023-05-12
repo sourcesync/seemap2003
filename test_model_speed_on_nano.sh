57b780a7b29560b8bc0f2b0cd68d55c74bef5a26
George Williams <george.williams@gmail.com>
2023-05-11 23:34:28 -0700

    sync before router switch

--- test_model_speed_on_nano.sh
+++ test_model_speed_on_nano.sh
@@ -3,7 +3,7 @@
 MODEL=$1
 
 echo "Copying your model to nano..."
-scp "${MODEL}" cuongwilliams@192.168.1.14:/home/cuongwilliams/
+scp "${MODEL}" cuongwilliams@192.168.1.9:/home/cuongwilliams/
 
 echo "Running model speed test on nano..."
-ssh cuongwilliams@192.168.1.14 -t /home/cuongwilliams/nano_model_test.sh "${MODEL}"
+ssh cuongwilliams@192.168.1.9 -t /home/cuongwilliams/nano_model_test.sh "${MODEL}"
